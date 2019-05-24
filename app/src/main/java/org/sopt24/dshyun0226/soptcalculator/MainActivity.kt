8edf7ebfaff3ff7f11fc6aa9bcf2a7a2cfb70768
Jahyun Kim <dshyun0226@gmail.com>
2019-05-24 14:39:57 +0900

    Empty Project

---
+++ app/src/main/java/org/sopt24/dshyun0226/soptcalculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package org.sopt24.dshyun0226.soptcalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
